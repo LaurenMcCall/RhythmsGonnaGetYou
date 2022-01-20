de8de70d76b54706fb067a89aa47c9b65cc2ba5b
Lauren McCall <lmmccolloch@gmail.com>
2022-01-20 17:28:12 -0500

    Inserted Band values

--- BandsAndAlbums.sql
+++ BandsAndAlbums.sql
@@ -25,3 +25,24 @@ CREATE TABLE "Songs"(
 "AlbumId" INTEGER REFERENCES "Albums" ("Id")
 );
 
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Genre", "IsSigned", "ContactName")
+VALUES ('ABBA', 'Sweden', 4, 'abba.com', 'pop', true, 'Tina' );
+
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Genre", "IsSigned", "ContactName")
+VALUES ('Spice Girls', 'England', 5, 'spiceupyourlife.com', 'pop', false, 'Gina' );
+
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Genre", "IsSigned", "ContactName")
+VALUES ('TLC', 'US', 3, 'waterfalls.com', 'r&b', false, 'Liz' );
+
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Genre", "IsSigned", "ContactName")
+VALUES ('Destinys Child', 'US', 4, 'destinyschild.com', 'r&b', false, 'Tina' );
+
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Genre", "IsSigned", "ContactName")
+VALUES ('Garbage', 'US', 4, 'garbage.com', 'rock', true, 'Barb' );
+
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Genre", "IsSigned", "ContactName")
+VALUES ('The Runaways', 'US', 9 , 'runaways.com', 'rock', false, 'Barb' );
+
+
+
+
