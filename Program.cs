ad7b9af820d970239a837e0f76c31f35a29c2d88
Lauren McCall <lmmccolloch@gmail.com>
2022-01-20 19:34:43 -0500

    created Band class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,24 @@
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Band
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Genre { get; set; }
+        public bool IsSigned { get; set; }
+        public string ContactName { get; set; }
     }
-  }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
 }
+
+
