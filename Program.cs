7fa192e4eedc42058427ac47c2272abd345eb23a
Lauren McCall <lmmccolloch@gmail.com>
2022-01-22 15:32:24 -0500

    Added PromptForString and PromptForInteger

--- Program.cs
+++ Program.cs
@@ -7,14 +7,22 @@ namespace RhythmsGonnaGetYou
         static void DisplayGreeting()
         {
             Console.WriteLine("");
-            Console.WriteLine("🥁🎤🎸🎹🥁🎤🎸🎹🥁🎤🎸🎹🥁🎤🎸🎹🥁🎤🎸🎹");
-            Console.WriteLine("     Welcome to the Music Database   ");
-            Console.WriteLine("🥁🎤🎸🎹🥁🎤🎸🎹🥁🎤🎸🎹🥁🎤🎸🎹🥁🎤🎸🎹");
+            Console.WriteLine("～*～♪ ～*～♪ ～*～♪ ～*～♪ ～*～♪ ～*～♪ ～*～");
+            Console.WriteLine("         Welcome to the Music Database   ");
+            Console.WriteLine("～*～♪ ～*～♪ ～*～♪ ～*～♪ ～*～♪ ～*～♪ ～*～");
             Console.WriteLine("");
         }
 
         static void DisplayMenu()
         {
+            Console.WriteLine("");
+            Console.WriteLine("Please make a selection: ");
+            Console.WriteLine("☆━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━☆");
+            Console.WriteLine("");
+
+
+
+
             // Add a new band
             // View all the bands
             // Add an album for a band
@@ -28,12 +36,39 @@ namespace RhythmsGonnaGetYou
             // Quit the program
         }
 
+        public static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        public static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer. ");
+                return 0;
+            }
+        }
+
         static void Main(string[] args)
         {
 
             var context = new RhythmsGonnaGetYouContext();
 
             DisplayGreeting();
+
+            DisplayMenu();
         }
     }
 }
